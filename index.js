0c3bcfbe3752f7fb3e59a087e9aa7f93afd461e6
Kevin Sanchez <Chilledsonic.ks2@gmail.com>
2023-08-14 11:32:40 -0400

    Added Animations

--- index.js
+++ index.js
@@ -1,6 +1,33 @@
-// template_3o70ddv
-// service_jxeokir
-// weLIEO56UxP9l6vMR
+let isModalOpen = false;
+let contrastToggle = false;
+const scaleFactor = 1 / 20;
+
+function moveBackground(event) {
+    const shapes = document.querySelectorAll(".shape")
+    const x = event.clientX * scaleFactor;
+    const y = event.clientY * scaleFactor;
+
+    for (let i = 0; i < shapes.length; ++i) {
+        const isOdd = i % 2 !== 0;
+        const boolInt = isOdd ? -1 : 1;
+        shapes[i].style.transform = `translate(${x * boolInt}px, ${y * boolInt}px)`
+    }
+}
+
+
+
+
+
+
+function toggleContrast () {
+    contrastToggle = !contrastToggle;
+    if (contrastToggle) {
+        document.body.classList += " dark-theme"
+    }
+    else {
+        document.body.classList.remove("dark-theme")
+    }
+}
 
 function contact(event) {
     event.preventDefault();
@@ -24,7 +51,6 @@ function contact(event) {
         })
 }
 
-let isModalOpen = false;
 function toggleModal() {
     if (isModalOpen) {
         isModalOpen = false;
@@ -33,3 +59,4 @@ function toggleModal() {
     isModalOpen = true;
     document.body.classList += " modal--open"
 }
+
